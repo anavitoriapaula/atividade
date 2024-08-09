75e6df380e9a384391a8c83064fbc28fb81666dd
anavitoriapaula <anavitoriapaulachavesifpi@gmail.com>
2024-08-09 14:33:05 -0300

    questao2.py

--- questao2.py
+++ questao2.py
@@ -1,11 +1,8 @@
 def imprimir_pa(primeiro_termo, quantidade_termos, razao):
-    # Calcula e imprime cada termo da PA
     for i in range(quantidade_termos):
         termo_atual = primeiro_termo + i * razao
         print(termo_atual, end=' ')
-    print()  # Para adicionar uma nova linha após imprimir todos os termos
-
-# Exemplo de uso
+    print()
 primeiro_termo = float(input("Informe o primeiro termo: "))
 quantidade_termos = int(input("Informe a quantidade de termos: "))
 razao = float(input("Informe a razão: "))
