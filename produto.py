d9e85fcfeef914347357e90c0e1b217fe278489e
anavitoriapaula <anavitoriapaulachavesifpi@gmail.com>
2024-08-23 14:28:32 -0300

    produto.py

---
+++ produto.py
@@ -0,0 +1,11 @@
+produtos []
+whlile True:
+produto = input("digite o nome do produto")
+produtos.append(produto)
+continuar = input("Deseja adicionar outro produto: ") (sim/não): ")
+  if continuar upper()!= "sim":
+    break
+pront("\nLista de produtos eletronocos: ")
+for produto in produtos:
+print(produto")
+
